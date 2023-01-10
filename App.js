1e2bae0416437410b58fc9a35f4bb3eb059c75cd
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-01-10 00:50:28 -0300

    Added the ImcContextProvider

--- App.js
+++ App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainContainer from './components/mainContainer';
+import ImcContextProvider from './context/ImcContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Text style={styles.title}>CALCULADORA IMC</Text>
-      <MainContainer />
-    </View>
+    <ImcContextProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Text style={styles.title}>CALCULADORA IMC</Text>
+        <MainContainer />
+      </View>
+    </ImcContextProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
