5c5ab9f7d573dfb0fe57ea061dcff1ec45733a6f
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-01-10 00:52:30 -0300

    new component created for error messages

---
+++ components/errorMessage.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { StyleSheet, Text} from "react-native";
+import { ImcContext } from "../context/ImcContext";
+
+export default function ErrorMessage() {
+    const { errorMsg } = useContext(ImcContext);
+    
+    return (
+        <Text style={styles.errorText}>{errorMsg}</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+    errorText: {
+        color: '#0fcf9e',
+        fontWeight: 'normal',
+        fontSize: 14,
+        textAlign: 'center',
+        marginTop: 14
+    }
+});
