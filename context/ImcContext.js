527ae213b7a787f2dd1da78a6368ed785d840f72
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-01-12 00:27:54 -0300

    imcCalculator update

--- context/ImcContext.js
+++ context/ImcContext.js
@@ -1,25 +1,49 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ImcContext = createContext();
 
 export default function ImcContextProvider({children}) {
-    const [height, setHeight] = useState(null);
     const [weight, setWeight] = useState(null);
+    const [height, setHeight] = useState(null);
     const [errorMsg, setErrorMsg] = useState("");
     const [imcValue, setImcValue] = useState(null);
     const [imcCategory, setImcCategory] = useState("");
     const [btnText, setBtnText] = useState("Calcular");
 
-    function imcCalculator() {
-        return setImcValue(2);
-    };
+    useEffect(() => {
+        setBtnText("Calcular");
+        setErrorMsg(null);
+    }, []);
 
+    function imcCalculator() {
+        const formattedHeight = height.replace(",", ".")
+        const imcResult = (weight/(formattedHeight*formattedHeight)).toFixed(2)
+        
+        setImcValue(imcResult)
+
+        if (imcResult < 18.5 ) {
+            setImcCategory("FALTA DE PESO")
+        } else if (imcResult >= 18.5 && imcResult < 25) {
+            setImcCategory("PESO NORMAL")
+        } else if (imcResult >= 25 && imcResult < 30) {
+            setImcCategory("SOBREPESO")
+        } else if (imcResult >= 30 && imcResult < 35) {
+            setImcCategory("OBESIDADE I")
+        } else if (imcResult >= 35 && imcResult < 40) {
+            setImcCategory("OBESIDADE II")
+        } else if (imcResult >= 40) {
+            setImcCategory("OBESIDADE III")
+        }     
+        
+        return [imcValue, imcCategory]
+    };
+    
     function validationForm() {
-        if(weight != null && height != null) {
+        if(weight != 0 && height != 0) {
             imcCalculator()
-            setHeight(null)
-            setWeight(null)
-            setErrorMsg("")
+            setErrorMsg(null)
+            setWeight(0)
+            setHeight(0)
             setBtnText("Calcular novamente")
             return
         };
@@ -27,6 +51,8 @@ export default function ImcContextProvider({children}) {
         setImcValue(null)
         setImcCategory(null)
         setErrorMsg("Preencha todos os campos*")
+        setBtnText("Calcular")
+        return
     };
 
     return (
@@ -34,7 +60,7 @@ export default function ImcContextProvider({children}) {
         value={{
             weight, setWeight, height, 
             setHeight, errorMsg, imcValue, 
-            imcCategory, btnText, validationForm
+            imcCategory, validationForm, btnText
         }}>
             {children}
         </ImcContext.Provider>
